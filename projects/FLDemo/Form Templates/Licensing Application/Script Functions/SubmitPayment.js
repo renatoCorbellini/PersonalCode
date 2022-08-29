0a3e900f1a3061304eae8f8e7b7207b06a3cd093
renatoCorbellini <101141377+renatoCorbellini@users.noreply.github.com>
2022-08-29 10:44:42 -0300

    Payment updates

--- projects/FLDemo/Form Templates/Licensing Application/Script Functions/SubmitPayment.js
+++ projects/FLDemo/Form Templates/Licensing Application/Script Functions/SubmitPayment.js
@@ -21,6 +21,21 @@ if (ccMonth && ccYear && ccNumber && ccCSC) {
   //expdate must be mmyy format for payment
   $("#expdate").val(expdate);
 
+  VV.Form.SetFieldValue(
+    "Total Paid",
+    VV.Form.GetFieldValue("Total Owed"),
+    false
+  );
+  VV.Form.SetFieldValue("Status", "Paid", true);
+  VV.Form.SetFieldValue("Transaction ID", "123456789", true);
+
+  var today = new Date();
+  VV.Form.SetFieldValue(
+    "Date Paid",
+    today.getMonth() + 1 + "/" + today.getDate() + "/" + today.getFullYear(),
+    true
+  );
+
   //locate the CC form and submit
   //   $("form[name='payflow']").submit();
 } else {
